a48210cb4ffaabd0f2dd0009a5059478287bb84c
LIANYCL <yucanlian@qq.com>
2019-11-30 14:50:54 +0800

    实现侧滑栏和下拉刷新

--- app/src/main/java/com/example/simplenews/Index.java
+++ app/src/main/java/com/example/simplenews/Index.java
@@ -1,35 +1,64 @@
 package com.example.simplenews;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-
+import android.widget.Toast;
 import com.example.simplenews.gson.Data;
 import com.example.simplenews.gson.News;
 import com.google.gson.Gson;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class Index extends AppCompatActivity {
     private ListView lvNews;
-    //声明Adapter作为listview的填充
     private NewsAdapter adapter;
     private List<Data> dataList;
+    private DrawerLayout mDrawerLayout;
+    private SwipeRefreshLayout swipeRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.tou);
+        }
+
+        final NavigationView navView =(NavigationView) findViewById(R.id.nav_view);
+        navView.setCheckedItem(R.id.nav_call);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        //新闻列表的获取
         lvNews = (ListView)findViewById(R.id.lvNews);
         dataList = new ArrayList<Data>();
         adapter = new NewsAdapter(this, dataList);
@@ -44,7 +73,18 @@ public class Index extends AppCompatActivity {
             }
         });
         sendRequestWithOKHttp();
+
+        //下拉刷新
+        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                sendRequestWithOKHttp();
+            }
+        });
     }
+
     private void sendRequestWithOKHttp(){
         new Thread(new Runnable() {
             @Override
@@ -62,6 +102,14 @@ public class Index extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                //更新Adapter(务必在主线程中更新UI!!!)
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.notifyDataSetChanged();
+                        swipeRefresh.setRefreshing(false);
+                    }
+                });
             }
         }).start();
     }
@@ -78,19 +126,33 @@ public class Index extends AppCompatActivity {
             String author_name = list.get(i).getAuthorName();
             String content_url = list.get(i).getUrl();
             String pic_url = list.get(i).getThumbnail_pic_s();
-//            System.out.println("标题："+title);
 //            System.out.println("日期："+date);
 //            System.out.println("作者："+author_name);
+//            System.out.println("标题："+title);
 //            System.out.println("网址："+content_url);
 //            System.out.println("图片："+pic_url);
             dataList.add(new Data(uniquekey, title, date, category, author_name, content_url, pic_url));
         }
-        //更新Adapter(务必在主线程中更新UI!!!)
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                adapter.notifyDataSetChanged();
-            }
-        });
     }
+
+    //加载菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return  true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.setting:
+                Toast.makeText(this, "You clicked Setting", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
 }
